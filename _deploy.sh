fca64129838928d31cc3ea0fe6d4444f9112b0aa
iorlov <nchanged@gmail.com>
2012-12-21 11:57:43 +0200

    Removing excessive files

--- _deploy.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-#--socks5-hostname 10.95.1.111:1080
-curl --cookie cookies.txt --cookie-jar newcookies.txt --user tomcat:1qw23er4123 --upload-file ./integration.war "http://tomcat:1qw23er4123@94.75.213.172:8080/manager/html/deploy?path=/integration&update=true"
